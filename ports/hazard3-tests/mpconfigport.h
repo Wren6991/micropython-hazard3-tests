76c8ae6223ce05fc3716e24c60785883006ea82c
Luke Wren <wren6991@gmail.com>
2026-07-31 12:06:45 +0100

    Slightly more full-featured interpreter for tests

--- ports/hazard3-tests/mpconfigport.h
+++ ports/hazard3-tests/mpconfigport.h
@@ -1,14 +1,9 @@
 #include <stdint.h>
+#include <alloca.h>
 
-// options to control how MicroPython is built
+#define MICROPY_CONFIG_ROM_LEVEL (MICROPY_CONFIG_ROM_LEVEL_EXTRA_FEATURES)
 
-// Use the minimal starting configuration (disables all optional features).
-#define MICROPY_CONFIG_ROM_LEVEL (MICROPY_CONFIG_ROM_LEVEL_CORE_FEATURES)
-
-// You can disable the built-in MicroPython compiler by setting the following
-// config option to 0.  If you do this then you won't get a REPL prompt, but you
-// will still be able to execute pre-compiled scripts, compiled with mpy-cross.
-#define MICROPY_ENABLE_COMPILER     (1)
+#define MICROPY_ENABLE_COMPILER           (1)
 
 #define MICROPY_QSTR_EXTRA_POOL           mp_qstr_frozen_const_pool
 #define MICROPY_ENABLE_GC                 (1)
@@ -16,22 +11,23 @@
 #define MICROPY_MODULE_FROZEN_MPY         (1)
 #define MICROPY_ENABLE_EXTERNAL_IMPORT    (1)
 
-#define MICROPY_HELPER_REPL         (1)
-#define MICROPY_REPL_INFO           (1)
+#define MICROPY_HELPER_REPL               (1)
+#define MICROPY_REPL_INFO                 (1)
 
 #define MICROPY_ALLOC_PATH_MAX            (256)
 
 // Use the minimum headroom in the chunk allocator for parse nodes.
 #define MICROPY_ALLOC_PARSE_CHUNK_INIT    (16)
 
+#define MICROPY_PY_SYS_STDFILES (0)
+#define MICROPY_PY_UCTYPES (0)
+#define MICROPY_STACK_CHECK (0)
+
 // type definitions for the specific machine
 
 typedef intptr_t mp_int_t; // must be pointer size
 typedef uintptr_t mp_uint_t; // must be pointer size
 typedef long mp_off_t;
-
-// We need to provide a declaration/definition of alloca()
-#include <alloca.h>
 
 #define MICROPY_HW_BOARD_NAME "hazard3-testbench"
 #define MICROPY_HW_MCU_NAME "Hazard3"
